74a5aafa48a345752364fb881c5e496e7d3311ae
yanliangzhao <584598008@qq.com>
2015-12-18 00:15:44 +0800

    My second application!

---
+++ src/main/java/SecondTest.java
@@ -0,0 +1,8 @@
+/**
+ * Created by yanliangzhao on 15/12/18.
+ */
+public class SecondTest {
+    public static  void main(String[] args){
+        System.out.println("my second application!");
+    }
+}
